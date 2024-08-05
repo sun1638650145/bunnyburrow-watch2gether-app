67bec78c759d0742a561cad1f026a4c2aad9c7d1
sun1638650145 <1638650145@qq.com>
2024-08-05 14:02:18 +0800

    Add `LoginView`.

---
+++ Watch2Gether/LoginView.swift
@@ -0,0 +1,24 @@
+//
+//  LoginView.swift
+//  Watch2Gether
+//
+//  Created by Steve R. Sun on 2024/8/5.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Text("一起看电影")
+                .bold()
+                .font(.largeTitle)
+                .foregroundColor(Color(hex: "#F9F9F9"))
+                .padding(10)
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
