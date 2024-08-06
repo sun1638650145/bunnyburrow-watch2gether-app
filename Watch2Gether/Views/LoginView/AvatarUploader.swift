a5e999661220e1d134df31e9228b93a108a15e51
sun1638650145 <1638650145@qq.com>
2024-08-06 15:08:30 +0800

    Update `AvatarUploader` View.

--- Watch2Gether/Views/LoginView/AvatarUploader.swift
+++ Watch2Gether/Views/LoginView/AvatarUploader.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 
 struct AvatarUploader: View {
+    /// 按钮悬停状态.
+    @State private var isHovered = false
+    
     var body: some View {
         Button(action: {
             // ...
         }, label: {
-            // ...
+            Image("AvatarUploader")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 100, height: 100)
+                .padding(5)
         })
         .buttonStyle(PlainButtonStyle())
+        .onHover(perform: { hovering in
+            isHovered = hovering
+            
+            /// 在悬停时使用手形光标.
+            if isHovered {
+                NSCursor.pointingHand.push()
+            } else {
+                NSCursor.pop()
+            }
+        })
     }
 }
 
