00b85fc0d6062d354d7a27e092c90b08f9270f3f
sun1638650145 <1638650145@qq.com>
2024-08-23 15:45:10 +0800

    Add `Streaming`.

---
+++ Watch2Gether/Model/Streaming.swift
@@ -0,0 +1,14 @@
+//
+//  Streaming.swift
+//  Watch2Gether
+//
+//  Created by Steve R. Sun on 2024/8/23.
+//
+
+import Foundation
+
+/// 流媒体视频源.
+struct Streaming {
+    /// 视频源url.
+    let url: URL
+}
