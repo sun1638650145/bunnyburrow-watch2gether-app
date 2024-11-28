ca778fbc663d75da66b4b5788688fef8e1922cfc
sun1638650145 <1638650145@qq.com>
2024-11-28 18:09:39 +0800

    Add `ChatInputTextField`.

--- Watch2Gether/Views/HomeView/ChatRoom.swift
+++ Watch2Gether/Views/HomeView/ChatRoom.swift
@@ -25,19 +25,7 @@ struct ChatRoom: View {
         Color.blue
         
         HStack {
-            TextField("", text: $message)
-                .frame(height: 35)
-                .padding(.leading, 5)
-                .background(Color(red: 249 / 255, green: 249 / 255, blue: 249 / 255, opacity: 0.1))
-                .clipShape(RoundedRectangle(cornerRadius: 8))
-                .foregroundStyle(Color(hex: "#E5E7EB"))
-                .onSubmit(sendMessage)
-                #if os(iOS)
-                /// 在iOS上设置提交按钮的标签为发送.
-                .submitLabel(.send)
-                #elseif os(macOS)
-                .textFieldStyle(PlainTextFieldStyle())
-                #endif
+            ChatInputTextField(text: $message, onTextSubmit: sendMessage)
             
             Button(action: sendMessage, label: {
                 Text("发送")
