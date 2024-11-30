49647a5941f8f77f8eafc4485c6f5718c125d904
sun1638650145 <1638650145@qq.com>
2024-11-30 19:07:21 +0800

    Add `MessagesList` & `MyMessage` & `OtherMessage` for `macOS`.

--- Watch2Gether/Views/HomeView/MessagesList.swift
+++ Watch2Gether/Views/HomeView/MessagesList.swift
@@ -26,6 +26,10 @@ struct MessagesList: View {
             }
         })
         .listStyle(PlainListStyle())
+        #if os(macOS)
+        /// 在macOS上隐藏列表内可滚动视图的背景.
+        .scrollContentBackground(.hidden)
+        #endif
     }
 }
 
