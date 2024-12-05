36e9eb33b37f5c2bc73266baf0980a42ea9b3819
sun1638650145 <1638650145@qq.com>
2024-12-05 17:11:43 +0800

    Add `Slider` for `PlaybackControls`.

--- Watch2Gether/Views/HomeView/VideoPlayer/PlaybackControls.swift
+++ Watch2Gether/Views/HomeView/VideoPlayer/PlaybackControls.swift
@@ -14,6 +14,9 @@ struct PlaybackControls: View {
     @Environment(User.self) var user
     @Environment(WebSocketClient.self) var websocketClient
     
+    /// 播放器进度条当前的位置.
+    @State private var seekPosition: Double = 0.0
+    
     /// `AVPlayer`播放器加载并控制视频播放.
     let player: AVPlayer
     
@@ -23,24 +26,62 @@ struct PlaybackControls: View {
     }
     
     var body: some View {
-        Button(action: {
-            if isPlaying {
-                player.pause()
-            } else {
-                player.play()
+        HStack {
+            Button(action: {
+                if isPlaying {
+                    player.pause()
+                } else {
+                    player.play()
+                }
+                
+                /// 设置播放状态.
+                isPlaying.toggle()
+                
+                sendPlayerSync(command: isPlaying ? "play": "pause")
+            }, label: {
+                Image(systemName: isPlaying ? "pause.fill": "play.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 20, height: 20)
+                    .foregroundStyle(Color(hex: "#F9F9F9"))
+            })
+            
+            Slider(value: $seekPosition, in: 0...1, onEditingChanged: { _ in
+                /// 获取当前播放的视频.
+                guard let currentVideo = player.currentItem
+                else {
+                    return
+                }
+                
+                /// 根据当前位置和视频总时长计算修改后的时间.
+                let targetTime = seekPosition * currentVideo.duration.seconds
+                
+                /// 修改播放进度.
+                player.seek(to: CMTime(seconds: targetTime, preferredTimescale: 1000))
+            })
+            .onAppear(perform: {
+                observePlayerProgress()
+            })
+        }
+    }
+    
+    /// 观察播放器的播放进度.
+    private func observePlayerProgress() {
+        player.addPeriodicTimeObserver(
+            /// 每隔0.5秒获取一次新的播放进度.
+            forInterval: CMTime(seconds: 0.5, preferredTimescale: 1000),
+            queue: nil,
+            using: { time in
+                /// 获取当前播放的视频.
+                guard let currentVideo = player.currentItem
+                else {
+                    return
+                }
+                
+                /// 计算新的进度条位置.
+                seekPosition = time.seconds / currentVideo.duration.seconds
             }
-            
-            /// 设置播放状态.
-            isPlaying.toggle()
-            
-            sendPlayerSync(command: isPlaying ? "play": "pause")
-        }, label: {
-            Image(systemName: isPlaying ? "pause.fill": "play.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 20, height: 20)
-                .foregroundStyle(Color(hex: "#F9F9F9"))
-        })
+        )
     }
     
     /// 发送播放器状态同步.
