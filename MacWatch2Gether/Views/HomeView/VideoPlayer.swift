a9141aba220bb40af3baa67eb5763f51aea2f526
sun1638650145 <1638650145@qq.com>
2024-12-09 17:43:00 +0800

    Migrate `VideoPlayer` states to `Streaming` for centralized management for `macOS`.

--- MacWatch2Gether/Views/HomeView/VideoPlayer.swift
+++ MacWatch2Gether/Views/HomeView/VideoPlayer.swift
@@ -15,9 +15,6 @@ struct VideoPlayer: View {
     @Environment(Streaming.self) var streaming
     @Environment(WebSocketClient.self) var websocketClient
     
-    /// 当前的播放速率.
-    @State private var currentPlaybackRate: Float = 1.0
-    
     var body: some View {
         ZStack {
             /// 使得视频播放器有更好的一体性.
@@ -43,7 +40,7 @@ struct VideoPlayer: View {
             if command == "play" {
                 /// 播放视频.
                 /// 不使用`player.play()`, 使用修改播放速率触发播放.
-                streaming.player.rate = currentPlaybackRate
+                streaming.player.rate = streaming.currentPlaybackRate
             } else if command == "pause" {
                 /// 暂停视频.
                 streaming.player.pause()
@@ -53,11 +50,11 @@ struct VideoPlayer: View {
             streaming.player.seek(to: CMTime(seconds: newProgress, preferredTimescale: 1000))
         } else if let playbackRate = command["playbackRate"].float {
             /// 调整播放速率.
-            currentPlaybackRate = playbackRate
+            streaming.currentPlaybackRate = playbackRate
             
             /// 修改播放速率会导致播放器立刻播放, 所以只能在播放器本身为播放状态时立刻播放.
             if streaming.player.timeControlStatus == .playing {
-                streaming.player.rate = currentPlaybackRate
+                streaming.player.rate = streaming.currentPlaybackRate
             }
         }
     }
