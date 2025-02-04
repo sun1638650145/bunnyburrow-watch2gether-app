e38f6c22e28cf13b1effd84573db7b7b3c1ace30
sun1638650145 <1638650145@qq.com>
2025-02-04 14:53:35 +0800

    Add `VideoPlayerModal` for `iPadOS` & `macOS`.

--- MacWatch2Gether/Views/HomeView/VideoPlayer/VideoPlayer.swift
+++ MacWatch2Gether/Views/HomeView/VideoPlayer/VideoPlayer.swift
@@ -16,8 +16,15 @@ import SwiftyJSON
 /// `VideoPlayer`是视频播放器视图, 显示视频内容并提供播放控制功能.
 struct VideoPlayer: View {
     @Environment(AppSettings.self) var appSettings
+    @Environment(FriendsViewModel.self) var friendsViewModel
     @Environment(StreamingViewModel.self) var streamingViewModel
     @Environment(WebSocketClient.self) var webSocketClient
+
+    /// 模态视图开关变量.
+    @State private var isModalOpen: Bool = false
+
+    /// 模态视图显示的通知信息变量.
+    @State private var notificationMessage: String = ""
 
     /// 视频播放进度条当前的位置(由`VideoPlayer`管理可以避免隐藏后重新显示播放控制栏时, 进度条位置被重置).
     @State private var seekPosition: Double = 0.0
@@ -36,10 +43,15 @@ struct VideoPlayer: View {
                 PlaybackControls(seekPosition: $seekPosition)
                     .padding(10)
             }
+
+            VideoPlayerModal(notificationMessage, isOpen: isModalOpen)
         }
         .onAppear(perform: {
-            /// 添加播放器状态同步事件监听器给WebSocket客户端.
+            /// 添加接收播放器状态同步和打开播放器模态框事件监听器给WebSocket客户端.
             webSocketClient.on(eventName: "receivePlayerSync", listener: self.receivePlayerSync(command:))
+            webSocketClient.on(eventName: "openModal", listener: { command, clientID in
+                self.openModal(command: command, clientID: clientID)
+            })
         })
         .onTapGesture(perform: {
             // TODO: 定时器无操作后自动消失(Steve).
@@ -73,17 +85,48 @@ struct VideoPlayer: View {
             }
         }
     }
+
+    /// 打开视频播放器模态框.
+    ///
+    /// - Parameters:
+    ///   - command: 状态同步命令字段.
+    ///   - clientID: 用户的客户端ID.
+    private func openModal(command: JSON, clientID: UInt) {
+        isModalOpen = true
+
+        /// 从好友信息视图模型中获取用户信息.
+        let friend = friendsViewModel.searchFriend(by: clientID)!
+
+        if let command = command.string {
+            if command == "play" {
+                notificationMessage = "用户\(friend.name)播放了当前内容."
+            } else if command == "pause" {
+                notificationMessage = "用户\(friend.name)暂停了当前内容."
+            }
+        } else if command["newProgress"].double != nil {
+            notificationMessage = "用户\(friend.name)修改了播放进度."
+        } else if command["playbackRate"].float != nil {
+            notificationMessage = "用户\(friend.name)修改了播放速率."
+        }
+
+        /// 设置模态框1秒钟后自动关闭.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: {
+            isModalOpen = false
+        })
+    }
 }
 
 #Preview {
     let appSettings = AppSettings()
     let user = User()
+    let friendsViewModel = FriendsViewModel()
     let streamingViewModel = StreamingViewModel(url: URL(string: "http://127.0.0.1:8000/video/oceans/")!)
     let webSocketClient = WebSocketClient()
 
     VideoPlayer()
         .environment(appSettings)
         .environment(user)
+        .environment(friendsViewModel)
         .environment(streamingViewModel)
         .environment(webSocketClient)
 }
