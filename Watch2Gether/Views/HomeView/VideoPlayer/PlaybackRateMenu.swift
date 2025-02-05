00a485fc386678d260d2c2f2da09f80903e70d18
sun1638650145 <1638650145@qq.com>
2025-02-05 19:51:13 +0800

    Update the functionality of `PlaybackRateMenu`.

--- Watch2Gether/Views/HomeView/VideoPlayer/PlaybackRateMenu.swift
+++ Watch2Gether/Views/HomeView/VideoPlayer/PlaybackRateMenu.swift
@@ -13,9 +13,25 @@ import SwiftUI
 struct PlaybackRateMenu: View {
     @Environment(StreamingViewModel.self) var streamingViewModel
 
+    /// 可供选择的播放速率.
+    private let playbackRates: [Float] = [0.5, 0.75, 1, 1.25, 1.5, 2]
+
     var body: some View {
         Menu(content: {
-            // ...
+            ForEach(playbackRates, id: \.self, content: { rate in
+                Button(action: {
+                    /// 调整播放速率.
+                    streamingViewModel.currentPlaybackRate = rate
+
+                    /// 修改播放速率会导致播放器立刻播放, 所以只能在播放器本身正在播放时直接修改播放速率.
+                    if streamingViewModel.isPlaying {
+                        streamingViewModel.player.rate = streamingViewModel.currentPlaybackRate
+                    }
+                }, label: {
+                    Text("\(rate.formattedPlaybackRate())倍")
+                })
+                .disabled(streamingViewModel.currentPlaybackRate == rate)
+            })
         }, label: {
             Text("\(streamingViewModel.currentPlaybackRate.formattedPlaybackRate())倍")
                 .foregroundStyle(Color.foreground)
