f09e13654d0d4bc17835cbf117951f57463e6c56
sun1638650145 <1638650145@qq.com>
2025-02-13 16:06:12 +0800

    Update the functionality of `formatTime`.

--- Watch2Gether/Views/HomeView/VideoPlayer/ProgressBar.swift
+++ Watch2Gether/Views/HomeView/VideoPlayer/ProgressBar.swift
@@ -64,11 +64,9 @@ struct ProgressBar: View {
         let minutes = totalSeconds % 3600 / 60
         let seconds = totalSeconds % 60
 
-        if hours > 0 {
-            return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
-        } else {
-            return String(format: "%02d:%02d", minutes, seconds)
-        }
+        return hours > 0
+            ? String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+            : String(format: "%02d:%02d", minutes, seconds)
     }
 }
 
