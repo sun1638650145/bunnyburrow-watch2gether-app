1ef7054c52429024a0ecf0ea9fd982be37b61788
sun1638650145 <1638650145@qq.com>
2025-02-14 17:50:06 +0800

    Implement automatic hiding of the `PlaybackControls` on `macOS`.

--- MacWatch2Gether/Views/HomeView/VideoPlayer/VideoPlayer.swift
+++ MacWatch2Gether/Views/HomeView/VideoPlayer/VideoPlayer.swift
@@ -51,7 +51,7 @@ struct VideoPlayer: View {
             })
         })
         .onTapGesture(perform: {
-            // TODO: 定时器无操作后自动消失(Steve).
+            streamingViewModel.resetHidePlaybackControlsTimer()
             streamingViewModel.showPlaybackControls.toggle()
         })
     }
