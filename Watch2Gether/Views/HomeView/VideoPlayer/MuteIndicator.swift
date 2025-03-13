87c85a0c95e93e21e8789e0d14d58339e1e712fd
sun1638650145 <1638650145@qq.com>
2025-03-13 14:46:02 +0800

    Add `MuteIndicator`.

---
+++ Watch2Gether/Views/HomeView/VideoPlayer/MuteIndicator.swift
@@ -0,0 +1,36 @@
+//
+//  Copyright © 2024-2025 Steve R. Sun. All rights reserved.
+//
+//  MuteIndicator.swift
+//  Watch2Gether
+//
+//  Created by Steve R. Sun on 2025/3/13.
+//
+
+import SwiftUI
+
+/// `MuteIndicator`是静音状态指示视图.
+struct MuteIndicator: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: "volume.slash.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .foregroundStyle(Color.foreground)
+                    .frame(width: 20, height: 20)
+
+                RoundedRectangle(cornerRadius: 2)
+                    .foregroundStyle(Color.foreground.opacity(0.2))
+                    .frame(width: 100, height: 4)
+            }
+            .padding(10)
+
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    MuteIndicator()
+}
