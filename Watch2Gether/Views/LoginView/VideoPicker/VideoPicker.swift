67615c5aa20b18e7347289f56d6fbafc3599460f
sun1638650145 <1638650145@qq.com>
2025-03-18 19:56:28 +0800

    Add `VideoPickerViewController`.

--- Watch2Gether/Views/LoginView/VideoPicker/VideoPicker.swift
+++ Watch2Gether/Views/LoginView/VideoPicker/VideoPicker.swift
@@ -9,11 +9,20 @@
 
 import SwiftUI
 
-/// `VideoPicker`是视频选择的视图, 允许用户选择一个本地视频文件.
+/// `VideoPicker`是用于视频选择的视图, 允许用户选择一个本地视频文件.
 struct VideoPicker: View {
+    @Binding var url: String?
+
+    /// 是否呈现`VideoPickerViewController`.
+    @State private var isPresented: Bool = false
+
+    init(_ url: Binding<String?>) {
+        self._url = url
+    }
+
     var body: some View {
         Button(action: {
-            // ...
+            isPresented = true
         }, label: {
             Image(systemName: "document.badge.plus.fill")
                 .resizable()
@@ -21,9 +30,14 @@ struct VideoPicker: View {
                 .frame(width: 30, height: 30)
                 .foregroundStyle(Color.foreground)
         })
+        .sheet(isPresented: $isPresented, content: {
+            VideoPickerViewController(selectedVideo: $url)
+        })
     }
 }
 
 #Preview {
-    VideoPicker()
+    @Previewable @State var url: String?
+
+    VideoPicker($url)
 }
---
+++ Watch2Gether/Views/LoginView/VideoPicker/VideoPickerViewController.swift
@@ -0,0 +1,54 @@
+//
+//  Copyright © 2024-2025 Steve R. Sun. All rights reserved.
+//
+//  VideoPickerViewController.swift
+//  Watch2Gether
+//
+//  Created by Steve R. Sun on 2025/3/18.
+//
+
+import SwiftUI
+import UniformTypeIdentifiers
+
+/// `VideoPickerViewController`是使用`UIDocumentPickerViewController`实现的视频选择视图控制器;
+/// 它允许用户从`文件App`中选择一个视频文件.
+struct VideoPickerViewController: UIViewControllerRepresentable {
+    @Binding var selectedVideo: String?
+
+    func makeUIViewController(context: Context) -> UIDocumentPickerViewController {
+        let viewController = UIDocumentPickerViewController(forOpeningContentTypes: [.movie])
+
+        /// 禁止用户选择多个文件.
+        viewController.allowsMultipleSelection = false
+
+        /// 委托`Coordinator`处理.
+        viewController.delegate = context.coordinator
+
+        return viewController
+    }
+
+    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: Context) {
+        // ...
+    }
+
+    class Coordinator: NSObject, UIDocumentPickerDelegate {
+        var parent: VideoPickerViewController
+
+        init(_ parent: VideoPickerViewController) {
+            self.parent = parent
+        }
+
+        func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+            guard let url = urls.first?.absoluteString
+            else {
+                return
+            }
+
+            self.parent.selectedVideo = url
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+}
