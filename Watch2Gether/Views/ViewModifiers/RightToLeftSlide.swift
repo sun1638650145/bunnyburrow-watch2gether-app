7c6f065e6b26ad5f4e84db38b5f6ba5da1919f43
sun1638650145 <1638650145@qq.com>
2025-07-01 15:44:06 +0800

    Refactor `RightToLeftSlide` using a `Timer`.

--- Watch2Gether/Views/ViewModifiers/RightToLeftSlide.swift
+++ Watch2Gether/Views/ViewModifiers/RightToLeftSlide.swift
@@ -7,10 +7,17 @@
 //  Created by Steve R. Sun on 2025/6/19.
 //
 
+import Foundation
 import SwiftUI
 
 /// `RightToLeftSlide`是用于使`View`从右向左滑动的视图修饰符.
 struct RightToLeftSlide: ViewModifier {
+    /// 用于控制滑动动画帧率的定时器.
+    @State private var animationTimer: Timer?
+
+    /// 视图所在容器的宽度.
+    @State private var containerWidth: CGFloat = 0.0
+
     /// 视图水平方向的偏移量.
     @State private var offset: CGFloat = 0.0
 
@@ -25,10 +32,26 @@ struct RightToLeftSlide: ViewModifier {
                     /// 设置视图初始位置.
                     offset = geometry.size.width
 
-                    withAnimation(.linear(duration: duration), {
-                        offset = -geometry.size.width
-                    })
+                    /// 获取视图所在容器的宽度.
+                    containerWidth = geometry.size.width
+
+                    beginAnimation()
                 })
+        })
+    }
+
+    /// 启动滑动动画, 通过定时器以120帧更新视图水平方向的偏移量.
+    private func beginAnimation() {
+        /// 每帧滑动的距离(此处简化计算, 当视图宽度为容器宽度时, 则最大移动距离为2倍容器宽度).
+        let distancePerFrame = (2 * containerWidth) / duration / 120
+
+        animationTimer = Timer.scheduledTimer(withTimeInterval: 1 / 120, repeats: true, block: { _ in
+            offset -= distancePerFrame
+
+            /// 当视图完全移出屏幕后, 关闭定时器.
+            if offset < -containerWidth {
+                animationTimer?.invalidate()
+            }
         })
     }
 }
