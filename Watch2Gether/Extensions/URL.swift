ea4b3ddef65fd395c7d1f9f7c311495d2a50cc0a
sun1638650145 <1638650145@qq.com>
2025-08-24 18:20:52 +0800

    Add `URL` Extension.

---
+++ Watch2Gether/Extensions/URL.swift
@@ -0,0 +1,25 @@
+//
+//  Copyright © 2024-2025 Steve R. Sun. All rights reserved.
+//
+//  URL.swift
+//  Watch2Gether
+//
+//  Created by Steve R. Sun on 2025/8/24.
+//
+
+import Foundation
+
+extension URL {
+    /// 返回域名URL.
+    var domainURL: URL? {
+        guard let scheme = self.scheme,
+              let host = self.host()
+        else {
+            return nil
+        }
+
+        let port = self.port
+
+        return URL(string: scheme + "://" + host + (port.map({ ":\($0)" }) ?? "") + "/")
+    }
+}
