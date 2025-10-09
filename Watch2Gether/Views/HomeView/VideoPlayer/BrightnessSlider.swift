b9b83be93a31000536757cd568613eceaa762591
sun1638650145 <1638650145@qq.com>
2025-10-09 11:37:55 +0800

    Add `BrightnessSlider`.

---
+++ Watch2Gether/Views/HomeView/VideoPlayer/BrightnessSlider.swift
@@ -0,0 +1,48 @@
+//
+//  Copyright © 2024-2025 Steve R. Sun. All rights reserved.
+//
+//  BrightnessSlider.swift
+//  Watch2Gether
+//
+//  Created by Steve R. Sun on 2025/10/9.
+//
+
+import SwiftUI
+
+/// `BrightnessSlider`是亮度滑块视图.
+struct BrightnessSlider: View {
+    /// 当前的亮度.
+    let brightness: Float
+
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: brightness < 0.5 ? "sun.min.fill" : "sun.max.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .foregroundStyle(Color.yellow)
+                    .frame(width: 20, height: 20)
+
+                ZStack(alignment: .leading, content: {
+                    /// 滑块的背景部分.
+                    RoundedRectangle(cornerRadius: 2)
+                        .foregroundStyle(Color.foreground.opacity(0.2))
+                        .frame(width: 100, height: 4)
+
+                    /// 滑块的前景部分(已滑过的部分).
+                    RoundedRectangle(cornerRadius: 2)
+                        .foregroundStyle(Color.volumeSliderForeground)
+                        .frame(width: CGFloat(brightness * 100), height: 4)
+                })
+            }
+            .padding(10)
+            .glassEffectCompat()
+
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    BrightnessSlider(brightness: 0.5)
+}
