184adf8182709fe721991ca630546e475442c2ab
sun1638650145 <1638650145@qq.com>
2025-10-30 17:45:01 +0800

    Add `setupConnection` to `WebSocketClient`.

--- Watch2Gether/Extensions/WebSocketClientExtension.swift
+++ Watch2Gether/Extensions/WebSocketClientExtension.swift
@@ -24,4 +24,21 @@ extension WebSocketClient {
             ]
         ])
     }
+
+    /// 配置WebSocket连接.
+    ///
+    /// - Parameters:
+    ///   - url: WebSocket服务地址.
+    ///   - user: 用户信息.
+    ///   - friendsViewModel: 好友信息视图模型.
+    func setupConnection(_ url: String, _ user: User, with friendsViewModel: FriendsViewModel) {
+        self.connect(url, user)
+        
+        self.on(eventName: "addFriend", listener: friendsViewModel.addFriend(friend:))
+        self.on(eventName: "hasFriend", listener: { (clientID: UInt) -> Bool in
+            return friendsViewModel.searchFriend(by: clientID) != nil
+        })
+        self.on(eventName: "offlineAllFriends", listener: friendsViewModel.offlineAllFriends)
+        self.on(eventName: "offlineFriend", listener: friendsViewModel.offlineFriend(by:))
+    }
 }
