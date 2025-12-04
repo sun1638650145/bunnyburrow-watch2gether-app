1b4e51155602a60a568d5fa894bd4037e3c41dea
sun1638650145 <1638650145@qq.com>
2025-12-04 19:01:48 +0800

    Optimize code.

--- Watch2Gether/Views/LoginView/VideoPickerTextField.swift
+++ Watch2Gether/Views/LoginView/VideoPickerTextField.swift
@@ -32,17 +32,6 @@ struct VideoPickerTextField: View {
     /// 占位文本的颜色.
     private let placeholderColor: Color
 
-    /// 显示在文本输入框中的文本(如果是文件URL且未在编辑则只显示文件名, 其他情况则显示完整的原始文本).
-    private var displayText: String {
-        /// 判断是否为文件URL且未处于编辑焦点.
-        guard let text = text, let url = URL(string: text), url.isFileURL, !isFocused
-        else {
-            return text ?? ""
-        }
-
-        return url.lastPathComponent
-    }
-
     init(
         _ placeholder: LocalizedStringResource,
         text: Binding<String?>,
@@ -67,20 +56,26 @@ struct VideoPickerTextField: View {
                     }
 
                     TextField("", text: Binding<String>(
-                        get: { displayText },
-                        set: {
-                            /// 未处于编辑焦点, 则保持完整的原始文本.
-                            guard isFocused else {
-                                return
-                            }
-
-                            text = $0.isEmpty ? nil : $0
-                        }
+                        get: { text ?? "" },
+                        set: { text = $0.isEmpty ? nil : $0 }
                     ))
                     .autocorrectionDisabled()
                     .focused($isFocused)
                     .foregroundStyle(Color.foreground)
                     .onChange(of: text, onTextChange)
+
+                    /// 判断是否为文件URL且未处于编辑焦点.
+                    if let text = text, let url = URL(string: text), url.isFileURL, !isFocused {
+                        Text(url.lastPathComponent)
+                            /// 用最大宽度和系统背景色覆盖文本输入框中的内容.
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .background(Color(UIColor.systemBackground))
+                            .foregroundStyle(Color.foreground)
+                            /// 使用单击手势替代按钮, 避免按钮默认动画造成的延迟感.
+                            .onTapGesture(perform: {
+                                isFocused = true
+                            })
+                    }
                 })
                 .padding(.leading, 10)
 
