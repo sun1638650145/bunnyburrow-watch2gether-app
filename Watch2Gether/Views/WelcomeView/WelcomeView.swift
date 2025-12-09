e8d3a4a995e48cd47c04e5de6c7d137a24cc8eeb
sun1638650145 <1638650145@qq.com>
2025-12-09 18:19:14 +0800

    Optimize code.

--- Watch2Gether/Views/WelcomeView/WelcomeView.swift
+++ Watch2Gether/Views/WelcomeView/WelcomeView.swift
@@ -84,26 +84,12 @@ struct WelcomeView: View {
             VStack {
                 Spacer()
 
-                if #available(iOS 26.0, *) {
-                    Button(action: handleLogin, label: {
-                        Text("Login")
-                            .bold()
-                            .font(.title2)
-                            .foregroundStyle(Color.foreground)
-                            .frame(width: 125, height: 36)
-                            .multilineTextAlignment(.center)
-                            .tracking(5)
-                    })
-                    .buttonStyle(GlassProminentButtonStyle())
-                    .padding(20)
-                } else {
-                    Button(action: handleLogin, label: {
-                        Text("Login")
-                            .frame(width: 150, height: 50)
-                    })
-                    .buttonStyle(LoginButtonStyle(isCapsuleShape: true))
-                    .padding(20)
-                }
+                Button(action: handleLogin, label: {
+                    Text("Login")
+                        .frame(width: 150, height: 50)
+                })
+                .buttonStyle(LoginButtonStyle())
+                .padding(20)
 
                 Text("Copyright © 2025 Steve R. Sun")
                     .copyright()
