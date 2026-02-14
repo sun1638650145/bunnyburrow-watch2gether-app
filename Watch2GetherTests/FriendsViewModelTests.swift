99a7cce59dcae103adc80da11d47b82f322e0b4b
sun1638650145 <1638650145@qq.com>
2026-02-14 11:43:05 +0800

    test: add `offlineAllFriends` and `searchFriend` unit tests for `FriendsViewModel`

--- Watch2GetherTests/FriendsViewModelTests.swift
+++ Watch2GetherTests/FriendsViewModelTests.swift
@@ -24,4 +24,41 @@ struct FriendsViewModelTests {
 
         #expect(onlineFriends.count == 1)
     }
+
+    @Test
+    func offlineAllFriends() {
+        let friendsViewModel = FriendsViewModel()
+        let user = User(clientID: 2026, name: "Steve")
+        let friend = User(clientID: 2025, name: "A")
+
+        friendsViewModel.addFriend(friend: user)
+        friendsViewModel.addFriend(friend: friend)
+
+        var onlineFriends = friendsViewModel.getOnlineFriendsList()
+
+        #expect(onlineFriends.count == 2)
+
+        friendsViewModel.offlineAllFriends()
+
+        onlineFriends = friendsViewModel.getOnlineFriendsList()
+
+        #expect(onlineFriends.count == 1, "调用函数`offlineAllFriends()`后, 应只剩自己保持在线状态.")
+        #expect(onlineFriends[0].clientID == user.clientID)
+        #expect(onlineFriends[0].name == user.name)
+    }
+
+    @Test
+    func searchFriend() {
+        let friendsViewModel = FriendsViewModel()
+        let user = User(clientID: 2026, name: "Steve")
+
+        friendsViewModel.addFriend(friend: user)
+
+        let friend1 = friendsViewModel.searchFriend(by: user.clientID)
+        let friend2 = friendsViewModel.searchFriend(by: 1)
+
+        #expect(friend1?.clientID == user.clientID)
+        #expect(friend1?.name == user.name)
+        #expect(friend2?.clientID == nil)
+    }
 }
