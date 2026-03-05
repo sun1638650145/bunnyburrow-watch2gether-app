71deb07b213ba133a503e9600f423bb23d7e2ce5
sun1638650145 <1638650145@qq.com>
2026-03-05 17:15:51 +0800

    style(tests): add blank lines to separate imports

--- Watch2GetherTests/Model/VersionTests.swift
+++ Watch2GetherTests/Model/VersionTests.swift
@@ -8,6 +8,7 @@
 //
 
 import Testing
+
 @testable import Watch2Gether
 
 struct VersionTests {
--- Watch2GetherTests/ViewModels/PlayerViewModelTests.swift
+++ Watch2GetherTests/ViewModels/PlayerViewModelTests.swift
@@ -9,6 +9,7 @@
 
 import Foundation
 import Testing
+
 @testable import Watch2Gether
 
 struct PlayerViewModelTests {
