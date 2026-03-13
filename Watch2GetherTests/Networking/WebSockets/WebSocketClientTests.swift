226346be2931d1181df6f7f93e350649d3c8071e
sun1638650145 <1638650145@qq.com>
2026-03-13 16:51:13 +0800

    test: add `reconnect` unit test for `WebSocketClient`

--- Watch2GetherTests/Networking/WebSockets/WebSocketClientTests.swift
+++ Watch2GetherTests/Networking/WebSockets/WebSocketClientTests.swift
@@ -80,4 +80,35 @@ struct WebSocketClientTests {
             #expect(Bool(false), "WebSocket消息应为字符串类型.")
         }
     }
+
+    @Test
+    func reconnect() {
+        let mockSocket = MockWebSocketTask()
+        let webSocketClient = WebSocketClient(makeSocket: { _ in
+            return mockSocket
+        })
+        let user = User(clientID: 2026, name: "Steve")
+
+        webSocketClient.connect("wss://example.com/ws/", user)
+        webSocketClient.disconnect()
+        webSocketClient.reconnect()
+
+        #expect(mockSocket.cancelCallCount == 1)
+        #expect(mockSocket.resumeCallCount == 2, "连接和重连时均会调用`resume()`.")
+        #expect(mockSocket.messages.count == 3, "连接, 断开和重连各广播一条消息.")
+
+        let message = mockSocket.messages.last
+        switch message {
+        case .string(let string):
+            let json = JSON(string.data(using: .utf8)!)
+
+            #expect(json["props"]["type"] == "websocket.broadcast")
+            #expect(json["data"]["action"] == "connect")
+            #expect(json["data"]["status"] == "login")
+            #expect(json["data"]["user"]["clientID"].uIntValue == user.clientID)
+            #expect(json["data"]["version"] == "1.1.0")
+        default:
+            #expect(Bool(false), "WebSocket消息应为字符串类型.")
+        }
+    }
 }
